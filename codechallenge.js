2867ddc0f940dce0c0c7640c37c27b814f35a09c
Alexis Romero <alexromero1995@hotmail.com>
2023-05-07 23:32:47 -0400

    added js practice challenge

---
+++ codechallenge.js
@@ -0,0 +1,10 @@
+function displayifChildIsAbleToRideTheRollerCoaster() {
+    var childHeight = 60
+    if (childHeight>52) {
+        console.log('Get on that ride, kiddo!')
+    }
+    else {
+        console.log('Sorry kiddo. Maybe next year.')
+    }
+}
+return displayifChildIsAbleToRideTheRollerCoaster()
